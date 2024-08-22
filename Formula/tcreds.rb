e3e2de8ce2bf34362609385ebc4fc3d9f6dea7b6
matus.vid <matusko.vid@gmail.com>
2024-08-22 20:59:07 +0200

    Update binary asset to alpha-1.0.2

--- Formula/tcreds.rb
+++ Formula/tcreds.rb
@@ -3,10 +3,10 @@ class Tcreds < Formula
     homepage "https://github.com/matusvid/tcreds"
     
     if OS.mac?
-      url "https://github.com/matusvid/tcreds/releases/download/alpha-1.0.1/tcreds-darwin-arm64"
+      url "https://github.com/matusvid/tcreds/releases/download/alpha-1.0.2/tcreds-darwin-arm64"
       sha256 "f496700905e1556cd15c7fc1054cf287472b573ad5f64f0a2521b9eb2e0848aa"
     elsif OS.linux?
-      url "https://github.com/matusvid/tcreds/releases/download/alpha-1.0.1/tcreds-linux-amd64"
+      url "https://github.com/matusvid/tcreds/releases/download/alpha-1.0.2/tcreds-linux-amd64"
       sha256 "2cd6c6061faf65eff906aa335e67123c1c87055b74f431542051f59d728906b5"
     end
   
